3fd668cb0c6b540e85c69649977b2867557cd064
yjooon <yjk7557@naver.com>
2022-12-27 00:04:57 +0900

    11047

--- Q11047/Q11047/Q11047.c
+++ Q11047/Q11047/Q11047.c
@@ -1,32 +1,29 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
 	int n = 0, k = 0;
 	scanf("%d %d", &n, &k);
 
 	int a = 0;
-	int* arr = (int*)malloc(sizeof(int) * n);	
+	int arr[10];
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a);
 		arr[i] = a;
 	}
 
 	int count = 0;
-	while (k > 0) {
-		for (int i = 0; i < n; i++) {
-			if (k >= arr[i] && k < arr[i + 1]) {
-				k -= arr[i];
-				count++;
-				if (k >= arr[i])
-					i--;
-			}
-		}		
-	}
+	for (int i = n - 1; i >= 0; i--) {
+		if (k >= arr[i]) {
+			count += k / arr[i];
+			k %= arr[i];
+		}
+		if (k == 0)
+			break;
+	}		
+
 	printf("%d", count);
 	
-	free(arr);
 	return 0;
 }
