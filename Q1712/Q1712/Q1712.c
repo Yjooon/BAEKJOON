a871efec7a8c9863fa285aeae71e009004fc29fd
yjooon <yjk7557@naver.com>
2022-12-28 01:16:27 +0900

    1712

---
+++ Q1712/Q1712/Q1712.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	int num = 0;
+	while (1) {
+		num++;
+		if (b >= c) {
+			num = -1;
+			break;
+		}
+		else {
+			c -= b;
+			num = a / c + 1;
+			break;			
+		}
+	}
+
+	printf("%d", num);
+	
+	
+
+	return 0;
+}
