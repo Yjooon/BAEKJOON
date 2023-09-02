3a01227106be361919987880232c0a008a8e6ee3
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2023-09-02 20:57:23 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+
+
+int main() {
+    FAST;
+    int n, m;
+    cin >> n >> m;    
+
+    cout << n - m;
+
+    return 0;
+}
