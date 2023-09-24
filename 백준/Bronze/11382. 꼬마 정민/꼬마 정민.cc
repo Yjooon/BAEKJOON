ed980828ac75f8ed794a831601db7c4b4847e0a7
YeongJoon <60257302+Yjooon@users.noreply.github.com>
2023-09-24 21:57:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+using namespace std;
+
+
+int main() {
+    FAST;
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << (long long)(a + b + c);
+    return 0;
+}
